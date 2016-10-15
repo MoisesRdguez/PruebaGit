d431f53bfe10447186fd341965e5082ff6c6e62e
MoisesRdguez <moises.rodriguez@uclm.es>
2016-10-15 15:00:42 +0200

    segundo commit

--- PruebaGit/PruebaGit/src/Ejemplo.java
+++ PruebaGit/PruebaGit/src/Ejemplo.java
@@ -4,6 +4,7 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una prueba de GitHub desde Eclipse");
+		System.out.println("Esto es una prueba de GitHub desde Eclipse");
 	}
 
 }
